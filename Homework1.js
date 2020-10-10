6b1d3ede8d5019210316203f922ed2adb7ff2763
MadImp <60294415+Mad-Imp@users.noreply.github.com>
2020-10-10 12:43:30 +0300

    Усовершенствовала код. Использовала spread оператор

--- Homework1.js
+++ Homework1.js
@@ -3,26 +3,26 @@
 const employers = ['Alex', '', 'ludmila', 'Victor', '', 'oleg', 'iNna', 'Ivan', 'Alex', 'Olga', ' Ann'];
 
 const employersNames = employers
-    .filter(employer => employer.length > 0)
+    .filter(employer => employer)
     .map(name => name.toLowerCase().trim());
 
-const sponsors = {
+let sponsors = {
     cash: [40000, 5000, 30400, 12000],
     eu: ['SRL', 'PLO', 'J&K'],
     rus: ['RusAuto', 'SBO']
 };
 
 const {cash, eu, rus} = sponsors;
-const {eu: [firstEuSp, secondEuSp, thirdEuSp]} = sponsors;
+const sumSponsors = [...eu, ...rus, 'unexpected sponsor'];
 
-function calcCash(own) {
-    return own.reduce((sum, current) => sum + current);
+const calcCash = (own = 0) => own.reduce((sum, current) => sum + current);
+
+const money = calcCash(cash);
+
+const makeBusiness = ({cash, emp, owner = 'Sam', director = 'Victor'}) => {
+    console.log(`We have a business. Owner: ${owner}, director: ${director}. Our budget: ${cash}. And our employers: ${emp}.
+And we have a sponsors: ${sumSponsors}.
+Note. Be careful with ${eu[0]}. It's a huge risk.`);
 }
 
-function makeBusiness(owner, cash, emp, director = 'Victor' ) {
-    console.log(`We have a business. Owner: ${owner}, director: ${director}. Our budget: ${cash}.
-And our employers: ${emp}.
-And we have a sponsors: ${eu}${rus},unexpected sponsor.
-Note. Be careful with ${firstEuSp}. It's a huge risk.`);
-}
-makeBusiness.apply(null, ['Sam', calcCash(cash), employersNames]);
+makeBusiness({cash: money, emp: employersNames});
